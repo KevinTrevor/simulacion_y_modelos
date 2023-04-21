ca815b3847c4685b310b6d03e3432cc107ff99e5
unknown <kevintrevor0905@gmail.com>
2023-04-21 16:59:08 -0400

    Test de promedio esperado añadido.

---
+++ throw_the_dice/dice_game.test.py
@@ -0,0 +1,12 @@
+import unittest
+from dice_game import play_the_game
+
+BET = 100
+EXPECTED_AVERAGE = 5
+
+class DiceGameTest(unittest.TestCase):
+    def test_expected_average(self):
+        self.assertLess(play_the_game(BET), EXPECTED_AVERAGE)
+
+if __name__ == "__main__":
+    unittest.main()
